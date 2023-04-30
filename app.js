b00b08c5156dd36e699146b4f98aa6890f7bd1a6
Gabriel Carlos Carvalho <gabrielcarlos@MacBook-Pro-de-Gabriel.local>
2023-04-29 23:24:47 -0400

    commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var app = require('./config/server');
+
+var server = app.listen(80, function(){
+    console.log('Servidor online');
+})
+
+var io = require('socket.io').listen(server);
+app.set('io', io);
+
+io.on('connection', function(socket){
+    console.log('Usuario conectou')
+
+    socket.on('disconnect', function(){
+        console.log('Usuario disconectou')
+    })
+
+    socket.on('msgParaServidor', function(data){
+        socket.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem})
+        socket.broadcast.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem})
+        
+    })
+
+})
+
+
+
